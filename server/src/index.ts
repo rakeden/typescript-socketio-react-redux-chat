3f7109263eed1c692cdcef40b5e7a1c031cbd9d0
rakeden <jannis.migdalas@gmail.com>
2017-05-26 23:43:04 +0200

    adding basic socket.io + bett dev workflow

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,20 @@
 import * as express from "express";
 import * as path from "path";
 import * as http from "http";
+import * as socketIo from "socket.io";
 
 class Server {
 
     public app: any;
     private server: any;
     private port: number;
+    private io: any;
 
     constructor() {
         this.port = 8008;
         this.createApp();
         this.createServer();
+        this.createSocket();
         this.listen();
     }
 
@@ -23,9 +26,20 @@ class Server {
         this.server = http.createServer(this.app);
     }
 
+    private createSocket(): void {
+        this.io = socketIo(this.server);
+    }
+
     private listen(): void {
         this.server.listen(this.port, () => {
             console.log('server listening on port %s', this.port);
+        })
+        this.io.on('connect', (socket: any) => {
+            console.log('Connected client on port %s.', this.port)
+
+            socket.on('disconnect', () => {
+                console.log('Client disconnected');
+            })
         })
     }
 
